7248be62c4c01a05bbf9f77f06cc558280ea53d6
Everton-8 <41762797+Everton-8@users.noreply.github.com>
2018-07-27 09:59:54 -0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,22 +1,22 @@
 #!/bin/bash
 if readlink /proc/$$/exe | grep -qs "dash"; then
-	echo "Esse script precisa ser executado com bash, não com sh"
+	echo "Esse script precisa ser executado com bash, nÃ£o com sh"
 	exit 1
 fi
 
 if [[ "$EUID" -ne 0 ]]; then
-	echo -e "\033[01;31m Desculpe, é necessário executar esse script como usuário root"
+	echo -e "\033[01;31m Desculpe, Ã© necessÃ¡rio executar esse script como usuÃ¡rio root"
 	exit 2
 fi
 
 if [[ ! -e /dev/net/tun ]]; then
-	echo "o dispositivo TUN não está disponpivel.
-por favor, ative o TUN no cloud para usar esta instalação"
+	echo "o dispositivo TUN nÃ£o estÃ¡ disponpivel.
+por favor, ative o TUN no cloud para usar esta instalaÃ§Ã£o"
 	exit 3
 fi
 
 if grep -qs "CentOS release 5" "/etc/redhat-release"; then
-	echo "CentOS 5 e anteriores não são suportados"
+	echo "CentOS 5 e anteriores nÃ£o sÃ£o suportados"
 	exit 4
 fi
 if [[ -e /etc/debian_version ]]; then
@@ -28,20 +28,20 @@ elif [[ -e /etc/centos-release || -e /etc/redhat-release ]]; then
 	GROUPNAME=nobody
 	RCLOCAL='/etc/rc.d/rc.local'
 else
-	echo "Parece que você não está executando isso no Debian, Ubuntu ou CentOS"
+	echo "Parece que vocÃª nÃ£o estÃ¡ executando isso no Debian, Ubuntu ou CentOS"
 	exit 5
 fi
 clear
 echo -e "\033[01;31m "
-echo "PRIMEIRO SERÁ INSTALADO O PAINEL DE GERENCIAMETO DO OPENVPN,"
+echo "PRIMEIRO SERÃ INSTALADO O PAINEL DE GERENCIAMETO DO OPENVPN,"
 echo "NET-8 VENHA SER LIVRE!"
 sleep 6
 clear
-read -p "DESEJA INICIAR A INSTALAÇÃO DO OPENVPN? (s|n): " solo
+read -p "DESEJA INICIAR A INSTALAÃ‡ÃƒO DO OPENVPN? (s|n): " solo
 if [ "$solo" = "s" ]; then
 clear
 rm /bin/ovpn/ &>/dev/null
-wget -O /bin/ovpn -o /dev/null https://raw.githubusercontent.com/Darkbot345/OVPN/master/ovpn
+wget -O /bin/ovpn -o /dev/null https://raw.githubusercontent.com/Everton-8/vps/master/ovpn.bash
 chmod +x /bin/ovpn
 clear
 sleep 1
